5a43f769653218f9e50945e5f461a08fd7c17ba0
snowflakely <info@snowflakely.com>
2025-04-30 20:07:55 +0900

    0430

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    원룸샵
+    <h4 class="red" :style="스타일">{{products[0]}}</h4>
+    <p>만원</p>
+  </div>
+  <div>
+    <h4>{{products[1]}}</h4>
+    <p>만원</p>
+  </div>
+  <div>
+    <h4>{{products[2]}}</h4>
+    <p>60만원</p>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data(){
+      return{
+        products :['역삼동원룸','천호동원룸','마포구원룸']
+      }
+    },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
