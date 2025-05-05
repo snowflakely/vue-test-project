9a1bf13df57c48ef554511c6ca0e8ab241daadd4
snowflakely <info@snowflakely.com>
2025-05-05 20:37:28 +0900

    0505

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,32 @@
   </div>
 
   <div v-for="(content,i) in products" :key="i">
-    <h4 class="red" :style="스타일">{{content[0]}}</h4>
+    <h4 class="red">{{content[0]}}</h4>
     <p>{{ content[1]}}만원</p>
+    <button v-on:click="report_plus(i)">허위매물신고</button> <span>신고수 : {{report[i]}}</span>
   </div>
+
 </template>
 
 <script>
 
 
+
 export default {
   name: 'App',
   data(){
+    const products = [['역삼동원룸','60'],['천호동원룸','50'],['마포구원룸','40']];
       return{
+        products,
         menus : ['Home','Shop','About'],
-        products :[['역삼동원룸','60'],['천호동원룸','50'],['마포구원룸','40']]
+        report : new Array(products.length).fill(0)
       }
     },
+  methods :{
+    report_plus(index){
+      this.report[index]++;
+    }
+  },
   components: {
     
   }
